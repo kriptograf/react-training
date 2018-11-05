859f4b483870c48a1c5d190a5e99b382ed740e98
foreach <foreach@mail.ru>
2018-11-05 22:58:39 +0300

    add interaction

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article(props){
-    const {article} = props
-    const body = <section>{article.text}</section>
-    return (
-        <div className='hello'>
-            <h2>{article.title}</h2>
-            {body}
-            <h4>created: {(new Date(article.date)).toDateString()}</h4>
-        </div>
-    )
+class Article extends Component{
+    /*constructor(props){
+        super(props)
+        this.state = {
+            isOpen = true
+        }
+    }*/
+
+    //Экспериментальный синтаксис
+    state = {
+        isOpen: false
+    }
+
+    //Обязательный метод
+    render(){
+        const {article} = this.props
+        const body = this.state.isOpen && <section>{article.text}</section>
+        return (
+            <div className='hello'>
+                <h2>
+                    {article.title}
+                    <button onClick={this.handleClose}>{this.state.isOpen ? 'close' : 'open'}</button>
+                </h2>
+                {body}
+                <h4>created: {(new Date(article.date)).toDateString()}</h4>
+            </div>
+        )
+    }
+
+    handleClose = () => {
+        console.log('click');
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
 
 export default Article
